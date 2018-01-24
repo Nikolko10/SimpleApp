9676b0e79e83814625b30373b9b1c8cf37146228
Nicky <kocakola2010@gmail.com>
2018-01-24 19:12:42 +0200

    initial

---
+++ jsx/blocks.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { SketchPicker } from 'react-color';
+
+const styleBlock = {
+	base: {
+		margin: '10px 10px',
+		width: '100px',
+		height: '100px',
+		border: '1px solid black',
+		cursor: 'pointer',
+		padding: '5px',
+		textAlign: 'center',
+		color: 'white'
+	},
+	bgc: {
+		backgroundColor: 'red'
+	}
+};
+
+export default class Blocks extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isShow: false
+		}
+	}
+
+	handleSwap() {
+		this.setState({
+			isShow: !this.state.isShow
+		})
+	}
+
+	editBlock(id, title, color) {
+		this.props.edit(id, title, color)
+		this.setState({
+			isShow: !this.state.isShow
+		})
+	}
+
+	getStatic(id, title, color) {
+		return <div onClick={this.handleSwap.bind(this)} style={Object.assign({}, styleBlock.base, styleBlock.bgc)}>{title}</div>
+	}
+
+	getEdit(id, title, color) {
+		return <div style={Object.assign({}, styleBlock.base, styleBlock.bgc)}>
+			<input type='text' ref='inputTitle' placaholder='Input title' defaultValue={title} />
+			<input type='text' ref='inputCollor' placaholder='Input color' defaultValue={color} />
+			<button onClick={() => this.editBlock(id, this.refs.inputTitle.value, this.refs.inputCollor.value)}>Save</button>
+			<button onClick={this.handleSwap.bind(this)}>Cencel</button>
+		</div>
+	}
+
+	render() {
+		const { id, title, color } = this.props
+		console.log(this.props)
+		styleBlock.bgc['backgroundColor'] = color;
+		return this.state.isShow === false ? this.getStatic(id, title, color) : this.getEdit(id, title, color)
+	}
+}
---
+++ jsx/main.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import Blocks from './blocks.jsx';
+
+const style = {
+	display: 'flex'
+}
+
+export default class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{
+					id: 1,
+					title: 'Hello 1',
+					color:'#bbbbbb'
+				},
+				{
+					id: 2,
+					title: 'Hello 2',
+					color:'red'
+				},
+				{
+					id: 3,
+					title: 'Hello 3',
+					color:'blue'
+				},
+				{
+					id: 4,
+					title: 'Hello 4',
+					color:'green'
+				},
+				{
+					id: 5,
+					title: 'Hello 5',
+					color:'pink'
+				},
+				{
+					id: 6,
+					title: 'Hello 6',
+					color:'#a125b9'
+				},
+				{
+					id: 7,
+					title: 'Hello 7',
+					color:'#ab78b5'
+				},
+				{
+					id: 8,
+					title: 'Hello 8',
+					color:'#564d52'
+				},
+				{
+					id: 9,
+					title: 'Hello 9',
+					color:'#d47e18'
+				},
+				{
+					id: 10,
+					title: 'Hello 10',
+					color:'#46392a'
+				}
+			]
+		}
+	}
+
+	setNewData(id, title, color) {
+		var newData = this.state.data;
+		newData.forEach(item => {
+			if (item.id === id) {
+				item.title = title;
+				item.color = color;
+			}
+		});
+		console.log(newData)
+		this.setState({
+			data: newData
+		})
+	}
+
+	render() {
+		return <div style={style}>
+			{
+				this.state.data.map((item, i) => {
+					return <Blocks key={i} id={item.id} title={item.title} color={item.color} edit={(id, title, color) => this.setNewData(id, title, color)} />
+				})
+			}
+		</div>
+	}
+}
